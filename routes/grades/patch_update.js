7ef4b714110e002df2d53135aaa9dc30de837015
Md-kalam <101163655+Md-kalam@users.noreply.github.com>
2023-06-16 23:10:30 +0530

    Update patch_update.js

--- routes/grades/patch_update.js
+++ routes/grades/patch_update.js
@@ -34,7 +34,6 @@ router.patch('/update', async function (req, res) {
                     
                 }
             });
-            return res.status(200).json({"status": 200, "message": "ok"})
         }
 
     } catch (error) {
